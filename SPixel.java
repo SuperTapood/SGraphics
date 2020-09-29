05ae1b7e3b3436873a5e749fa4bdea1b6766764d
ShZil <shzil333@gmail.com>
2020-09-29 23:05:19 +0300

    Base files

---
+++ SPixel.java
@@ -0,0 +1,29 @@
+class SPixel {
+	SColor color;
+	int x;
+	int y;
+
+	public SPixel(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.color = SColor.BLACK;
+	}
+
+	public SPixel(int x, int y, SColor color) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
+
+	public SPixel(Pt loc) {
+		this.x = loc.x;
+		this.y = loc.y;
+		this.color = SColor.BLACK;
+	}
+
+	public SPixel(Pt loc, SColor color) {
+		this.x = loc.x;
+		this.y = loc.y;
+		this.color = color;
+	}
+}
