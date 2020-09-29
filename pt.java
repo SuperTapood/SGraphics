894144202f0c0bb5a6cd54ba1102b38840b0cf97
ShZil <shzil333@gmail.com>
2020-09-29 23:34:03 +0300

    Pt additions, start of MainExample, Scale

--- pt.java
+++ pt.java
@@ -1,3 +1,55 @@
 class Pt {
-	
+	int x;
+	int y;
+
+	public Pt(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Pt(int[] loc) {
+		this.x = loc[0];
+		this.y = loc[1];
+	}
+
+	public Pt(Pt other) {
+		this.x = other.x;
+		this.y = other.y;
+	}
+
+	public void add(Pt other) {
+		this.x += other.x;
+		this.y += other.y;
+	}
+
+	public void add(int x, int y) {
+		this.add(new Pt(x, y));
+	}
+
+	public void add(int[] loc) {
+		this.add(new Pt(loc));
+	}
+
+	public void sub(Pt other) {
+		this.x -= other.x;
+		this.y -= other.y;
+	}
+
+	public void sub(int x, int y) {
+		this.sub(new Pt(x, y));
+	}
+
+	public void sub(int[] loc) {
+		this.sub(new Pt(loc));
+	}
+
+	public void mult(Scale scale) {
+		this.x *= scale.x;
+		this.y *= scale.y;
+	}
+
+	public void div(Scale scale) {
+		this.x /= scale.x;
+		this.y /= scale.y;
+	}
 }
---
+++ scale.java
@@ -0,0 +1,39 @@
+class Scale {
+  float x;
+  float y;
+
+  public Scale(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+  public Scale(float s) {
+		this.x = s;
+		this.y = s;
+	}
+
+	public Scale(float[] loc) {
+		this.x = loc[0];
+		this.y = loc[1];
+	}
+
+  public Scale(int x, int y) {
+		this.x = (float)x;
+		this.y = (float)y;
+	}
+
+  public Scale(int s) {
+		this.x = (float)s;
+		this.y = (float)s;
+	}
+
+	public Scale(int[] loc) {
+		this.x = (float)loc[0];
+		this.y = (float)loc[1];
+	}
+
+	public Scale(Scale other) {
+		this.x = other.x;
+		this.y = other.y;
+	}
+}
