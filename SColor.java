7ab69955288e4c1b10b609c428ddfcb526efa2fe
SuperTapood <yoav.o252@gmail.com>
2020-09-30 09:09:38 +0300

    added grayscale constructors

--- SColor.java
+++ SColor.java
@@ -18,4 +18,17 @@ class SColor {
 		this.g = rgb[1];
 		this.b = rgb[2];
 	}
+
+	public SColor(float gray){
+		staticColor = SColor.FloatToInt(gray);
+		this.r = staticColor;
+		this.g = staticColor;
+		this.b = staticColor;
+	}
+
+	public SColor(int gray){
+		this.r = gray;
+		this.g = gray;
+		this.b = gray;
+	}
 }
