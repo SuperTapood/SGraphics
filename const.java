555c16b77e28a16dbc8e36ffe0ea4969fb1a1004
ShZil <shzil333@gmail.com>
2020-10-11 16:03:50 +0300

    Now, Const.WHITE is white and not black

--- const.java
+++ const.java
@@ -1,7 +1,7 @@
 class Const {
   public static final String PROJECT_NAME = "Smart Graphics";
   public static final SColor BLACK = new SColor(0f);
-  public static final SColor WHITE = new SColor(0f);
+  public static final SColor WHITE = new SColor(1f);
   public static final short ARRAY_FORMAT_SAME_ELEMENT_LIMIT = 3;
 
   public static final String THE_VERY_BEST = "Yoav Oren";
