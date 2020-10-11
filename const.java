5e70aa6616c0f9bbefffc5cef938406aa148bd48
ShZil <shzil333@gmail.com>
2020-10-11 22:24:04 +0300

    Added more colors

--- const.java
+++ const.java
@@ -2,6 +2,15 @@ class Const {
   public static final String PROJECT_NAME = "Smart Graphics";
   public static final SColor BLACK = new SColor(0f);
   public static final SColor WHITE = new SColor(1f);
+  public static final SColor GREY = new SColor(0.5f);
+  public static final SColor GRAY = new SColor(0.5f);
+  public static final SColor LIGHT_GREY = new SColor(0.2f);
+  public static final SColor LIGHT_GRAY = new SColor(0.2f);
+  public static final SColor DARK_GREY = new SColor(0.8f);
+  public static final SColor DARK_GRAY = new SColor(0.8f);
+  public static final SColor RED = new SColor(1f, 0f, 0f);
+  public static final SColor GREEN = new SColor(0f, 1f, 0f);
+  public static final SColor BLUE = new SColor(0f, 0f, 1f);
   public static final short ARRAY_FORMAT_SAME_ELEMENT_LIMIT = 3;
 
   //public static final String THE_VERY_BEST = "Yoav Oren";
