04ff7926cfe6296c5c4af3b2188ea8df3fb0a2fb
SuperTapood <yoav.o252@gmail.com>
2020-10-11 08:42:49 +0300

    updated

--- const.java
+++ const.java
@@ -4,6 +4,6 @@ class Const {
   public static final SColor WHITE = new SColor(0f);
   public static final short ARRAY_FORMAT_SAME_ELEMENT_LIMIT = 3;
 
-  public static final String THE_VERY_BEST = "Yoav Oren";
-  public static final String EVEN_BETTER = "ShZil";
+  //public static final String THE_VERY_BEST = "Yoav Oren";
+  //public static final String EVEN_BETTER = "ShZil";
 }
